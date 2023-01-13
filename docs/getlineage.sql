252f2e3c566fa2cd8e1a028beb77e175a09e966d
Sebastian Jaenicke <sjaenick@CeBiTec.Uni-Bielefeld.DE>
2023-01-13 12:50:43 +0100

    update sql query for lineage

--- docs/getlineage.sql
+++ docs/getlineage.sql
@@ -6,8 +6,22 @@ WITH RECURSIVE hierarchy AS (
   FROM taxdata t, hierarchy h
   WHERE t.taxid=h.parent_id
 )
-SELECT h.taxid, h.parent_id, t.name, t.rank, t.cutoff FROM hierarchy h
+SELECT h.taxid, h.parent_id, t.name, t.rank FROM hierarchy h
   LEFT JOIN taxdata t on h.taxid=t.taxid;
+
+
+slower:
+
+WITH RECURSIVE hierarchy AS (
+  SELECT taxid, parent_id, name, rank FROM taxdata WHERE taxid=2675216
+  UNION ALL
+  SELECT t.taxid, t.parent_id, t.name, t.rank
+  FROM taxdata t, hierarchy h
+  WHERE t.taxid=h.parent_id
+)
+SELECT h.taxid, h.parent_id, h.name, h.rank FROM hierarchy h;
+
+
 
 CREATE UNIQUE INDEX id_idx ON taxdata(taxid, parent_id);
 
